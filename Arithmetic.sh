2994f1e3910aa77635e3b8ab7e19f4bf99c2b251
Saad <skamil60@gmail.com>
2020-07-07 23:00:39 +0530

    Stored Dictionary Values In Array

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -1,6 +1,9 @@
+#!/bin/bash -x
+
 echo "Welcome"
 
 declare -A results
+
 
 read -p "Enter a number : " a
 read -p "Enter 2nd number :" b
@@ -11,7 +14,9 @@ results[answer2]=$(($a * $b + $c))
 results[answer3]=$(($c + $a / $b))
 results[answer4]=$(($a % $b + $c))
 
+for (( i=1;i<=4;i++ ))
+do
+	resultarray[i]=${results[answer$i]}
+done
 
-
-echo ${results[@]}
-
+echo ${resultarray[@]}
