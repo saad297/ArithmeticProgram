901d6fdb9189ecaf1cb57d5b3fcf5402b46eb609
Saad <skamil60@gmail.com>
2020-07-07 22:56:54 +0530

    Results Stored In Dictionary

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -1,12 +1,17 @@
 echo "Welcome"
 
+declare -A results
+
 read -p "Enter a number : " a
 read -p "Enter 2nd number :" b
 read -p "Enter 3rd number : " c
 
-result1=$(($a + $b * $c))
-result2=$(($a * $b + $c))
-result3=$(($c + $a / $b))
-result4=$(($a % $b + $c))
+results[answer1]=$(($a + $b * $c))
+results[answer2]=$(($a * $b + $c))
+results[answer3]=$(($c + $a / $b))
+results[answer4]=$(($a % $b + $c))
 
+
+
+echo ${results[@]}
 
